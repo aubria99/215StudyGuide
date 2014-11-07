1ce9e5895812f7f120997ef45b306c971578f642
odemwingie705 <carlette.odemwingie@gmail.com>
2014-11-07 09:27:49 -0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -4,6 +4,29 @@
 #include <iostream> //this is what you need in every c++ program
 using namespace std; //using standard library
 
+int CompSci111(){
+    
+    int computerchoice; //initializes the variable computerchoice
+    srand(time(0)); //this initializes a random variable
+    computerchoice = (rand()%40)+1; //this sets the computerchoice equal to the random number generator
+    return computerchoice;
+}
+
+int CompSci121(){
+    
+    int computerchoice; //initializes the variable computerchoice
+    srand(time(0)); //this initializes a random variable
+    computerchoice = (rand()%40)+40; //this sets the computerchoice equal to the random number generator
+    return computerchoice;
+}
+
+int CompSci215(){
+    
+    int computerchoice; //initializes the variable computerchoice
+    srand(time(0)); //this initializes a random variable
+    computerchoice = (rand()%40)+80; //this sets the computerchoice equal to the random number generator
+    return computerchoice;
+}
 int main() //main function
 {
     string Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15, Q16, Q17, Q18, Q19, Q20, Q21, Q22, Q23, Q24, Q25, Q26, Q27, Q28, Q29, Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37,Q38, Q39, Q40, Q41, Q42, Q43, Q44, Q45, Q46, Q47, Q48, Q49, Q50, Q51, Q52, Q53, Q54, Q55, Q56, Q57, Q58, Q59, Q60, Q61, Q62, Q63, Q64, Q65, Q66, Q67, Q68, Q69, Q70, Q71, Q72, Q73, Q74, Q75, Q76, Q77, Q78, Q79, Q80, Q81, Q82, Q83, Q84, Q85, Q86, Q87, Q88, Q89, Q90, Q91, Q92, Q93, Q94, Q95, Q96, Q97, Q98, Q99, Q100, Q101, Q102, Q103, Q104, Q105, Q106, Q108, Q107, Q109, Q110, Q111, Q112, Q113, Q114, Q115, Q116, Q117, Q118, Q119, Q120; //list of strings which represent our questions
@@ -204,9 +227,10 @@ int main() //main function
     for (int i = 0; i < 10 && score < 7; i++) //start of for loop
     {
         
-        int computerchoice; //initializes the variable computerchoice
-        srand(i); //this initializes a random variable
-        computerchoice = (rand()%40)+1; //this sets the computerchoice equal to the random number generator
+        int computerchoice = CompSci111();
+        //we need to make an array to now store the number that the generator called and 
+        //check to make sure that whatever number is being called now isn't in the arry....can  
+        //arrays be dynamic?? if not. we may have to use linked list :/
         QA = question[computerchoice];
         cout<<computerchoice<<endl;
         cout<<endl<<QA<<endl;//displays the computer choice
