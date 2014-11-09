171d588f9e01a1f57f637281d87dfb587e1b7f0b
odemwingie705 <carlette.odemwingie@gmail.com>
2014-11-08 20:47:35 -0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -33,36 +33,36 @@ int main() //main function
     
     Q1 = "All the operations used to construct algorithms belong to one of only three categories: \n1.    Sequential \n2.    Conditional \nWhat is the third operation?\n\n"; //question 1
     
-    Q2 ="What is the best definition of an algorithm?\nA. A well-ordered collection of unambiguous and effectively computable operations that, when executed, produces a result and halts in a finite amount of time.\nB. A list of instructions that can be effectively computable or it can be unsolveable.\nC. Instructions to accomplish a task.\nD. Computer language.\n"; //question 2
+    Q2 = "What is the best definition of an algorithm?\nA. A well-ordered collection of unambiguous and effectively computable operations that, when executed, produces a result and halts in a finite amount of time.\nB. A list of instructions that can be effectively computable or it can be unsolveable.\nC. Instructions to accomplish a task.\nD. Computer language.\n"; //question 2
     
-    Q3 ="What generation were transistors and core memories invented?\nA.     Second\nB.     First\nC.     Fifth\nD.    Third\n"; //question 3
+    Q3 = "What generation were transistors and core memories invented?\nA.     Second\nB.     First\nC.     Fifth\nD.    Third\n"; //question 3
     
     Q4 ="a "; //question 4
     Q5 =" a"; //question 5
     Q6 =" a"; //question 6
     Q7 =" a"; //question 7
     
-    Q8 ="Any operation on this computer that exceeds the maximum allowable value is called ___________ _________.\n"; //question 8
+    Q8 = "Any operation on this computer that exceeds the maximum allowable value is called ___________ _________.\n"; //question 8
     
-    Q9 ="In an  __________ representation, objects can take on any value.\n"; //question 9
+    Q9 = "In an  __________ representation, objects can take on any value.\n"; //question 9
     
-    Q10 ="The __________ of the wave is a measure of its loudness—the greater the ________ the louder the sound.\n"; //question 10
-    Q11 ="The construction of computer circuits is based on the branch of mathematics and symbolic logic called _________   __________.\n"; //question 11
-    Q12 ="A _______ is an electronic device that operates on a collection of binary inputs to produce a binary output.\n"; //question 12
-    Q13 ="A _______ is a collection of logic gates that transforms a set of binary inputs into a set of binary outputs and in which the values of the outputs depend only on the current values of the inputs.\n"; //question 13
-    Q14 ="The study of algorithms is _________	___________.\n"; //question 14
-    
-    
+    Q10 = "The __________ of the wave is a measure of its loudness—the greater the ________ the louder the sound.\n"; //question 10
+    Q11 = "The construction of computer circuits is based on the branch of mathematics and symbolic logic called _________   __________.\n"; //question 11
+    Q12 = "A _______ is an electronic device that operates on a collection of binary inputs to produce a binary output.\n"; //question 12
+    Q13 = "A _______ is a collection of logic gates that transforms a set of binary inputs into a set of binary outputs and in which the values of the outputs depend only on the current values of the inputs.\n"; //question 13
+    Q14 = "The study of algorithms is _________	___________.\n"; //question 14
     
     
-    Q15 ="The study of the efficiency of algorithms is called _________  __ __________.\n"; //question 15
+    
+    
+    Q15 = "The study of the efficiency of algorithms is called _________  __ __________.\n"; //question 15
     Q16 =" ";
-    Q17 ="A  _______ can be thought of as a group of one or more, programming statements that collectively has a name.\n"; //question17
-    Q18 =" a"; //question 18
-    Q19 ="Every complete statement ends with a\nA)   Period\nB)   # symbol\nC)   semicolon\nD)   ending brace"; //question 19
-    Q20 ="Which of the following statements is correct?\nA)   #include (iostream)\nB)   #include {iostream}\nC)   #include <iostream>\nD)   #include [iostream]\nE)   All of the above\n"; //question 20
-    Q21 ="A _________ _____ is a statement that causes a function to execute. A function definition contains the statements that make up the function.\n"; //question 21
-    Q22 ="The ___ statement can cause other statements to execute only under certain conditions.\n"; //question 22
+    Q17 = "A  _______ can be thought of as a group of one or more, programming statements that collectively has a name.\n"; //question17
+    Q18 = " a"; //question 18
+    Q19 = "Every complete statement ends with a\nA)   Period\nB)   # symbol\nC)   semicolon\nD)   ending brace"; //question 19
+    Q20 = "Which of the following statements is correct?\nA)   #include (iostream)\nB)   #include {iostream}\nC)   #include <iostream>\nD)   #include [iostream]\nE)   All of the above\n"; //question 20
+    Q21 = "A _________ _____ is a statement that causes a function to execute. A function definition contains the statements that make up the function.\n"; //question 21
+    Q22 = "The ___ statement can cause other statements to execute only under certain conditions.\n"; //question 22
     Q23 =" a"; //question 23
     Q24 ="a "; //question 24
     
@@ -71,16 +71,16 @@ int main() //main function
     
     Q25 ="_______ ______ is breaking a program up into smaller, manageable functions. \n"; //question 25
     Q26 ="A ________ collection of statements to perform a specific task.\n"; //question 26
-    Q27 =": __________: Values passed to a function.\n"; //question 27
+    Q27 ="__________: Values passed to a function.\n"; //question 27
     Q28 ="__________: Variables in function that hold values passed as arguments.\n"; //question 28
     Q29 ="_____  ___ _____: when an argument is passed to a function, a copy of its value is placed in the parameter.\n"; //question 29
     Q30 ="______  ______: defined within a function or block; accessible only with the function or block.\n"; //question 30
     Q31 ="______  ______: a variable defined outside all functions; it is accessible to all functions within its scope.\n"; //question 31
-    Q32 =" _______  ______: a named constant that can be used by every function in a program.\n"; //question 32
+    Q32 ="_______  ______: a named constant that can be used by every function in a program.\n"; //question 32
     Q33 ="______ ______: values passed automatically if arguments are missing from the function call\n"; //question 33
-    Q34 ="_______  ______: an alias for another variable.Mechanism that allows a function to work with the original argument from the function call, not a copy of the argument\n"; //question 34
+    Q34 ="_______  ______: an alias for another variable. Mechanism that allows a function to work with the original argument from the function call, not a copy of the argument\n"; //question 34
     Q35 ="True or false: 4 Things Before A Compiler Runs it Must Know About a Function :\n1) name\n2) return type\n3) number of parameters\n4) data type of each parameter\n"; //question 35
-    Q36 ="True or false: \n The address operator (&) returns the memory address of a variable.\n"; //question 36
+    Q36 ="True or false: \nThe address operator (&) returns the memory address of a variable.\n"; //question 36
     Q37 ="a "; //question 37
     
     map<int, string> question; //this is passing in strings as vectors that will be used for the question
@@ -213,7 +213,7 @@ int main() //main function
     question[118] = Q118;
     question[119] = Q119;
     question[120] = Q120;
-
+    
     
     //these are the answers to the questions that are mapped at specific key values which are denoted in []
     answer[Q1] = "iterative"; //this is the answer to question 1
@@ -247,14 +247,14 @@ int main() //main function
     answer[Q29] = "pass by value"; //this is the answer to question 29
     answer[Q30] = "local variable"; //this is the answer to question 30
     answer[Q31] = "global variable"; //this is the answer to question 31
-    answer[Q32] = "global constants"; //this is the answer to question 32
+    answer[Q32] = "global constant"; //this is the answer to question 32
     answer[Q33] = "default arguments"; //this is the answer to question 33
     answer[Q34] = "refence variable"; //this is the answer to question 34
     answer[Q35] = "true"; //this is the answer to question 35
     answer[Q36] = "true"; //this is the answer to question 36
     answer[Q37] ="a"; //this is the answer to question 37
     
-     int score;
+    int score;
     int count = score =0;
     string userinput, username, correctchoice, QA; //this initializes the strings userinput and correctchoice
     cout<< "****************************************************************" <<endl;
